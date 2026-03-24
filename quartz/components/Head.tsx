6d4f9b84861ccfd2de836d5e49a298888d4d9b9a
mini <mini@miniui-MacBookPro.local>
2026-03-24 20:13:08 +0900

    Quartz sync: Mar 24, 2026, 8:13 PM

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -86,6 +86,34 @@ export default (() => {
         <meta name="description" content={description} />
         <meta name="generator" content="Quartz" />
 
+        {/* Schema.org Structured Data */}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Article",
+              headline: title,
+              description: description,
+              url: socialUrl,
+              author: {
+                "@type": "Person",
+                name: cfg.pageTitle,
+              },
+              publisher: {
+                "@type": "Organization",
+                name: cfg.pageTitle,
+              },
+              ...(fileData.dates?.modified && {
+                dateModified: fileData.dates.modified.toISOString(),
+              }),
+              ...(fileData.dates?.created && {
+                datePublished: fileData.dates.created.toISOString(),
+              }),
+            }),
+          }}
+        />
+
         {css.map((resource) => CSSResourceToStyleElement(resource, true))}
         {js
           .filter((resource) => resource.loadTime === "beforeDOMReady")
